2caf51d717809054edca4e477cfe40ac6d06d3b9
franciscorafaelpires <franciscorafaelpires@gmail.com>
2025-02-20 18:48:51 -0300

    primeiro commit

---
+++ exerciciosDeFixacaoRJ1.js
@@ -0,0 +1,265 @@
+let guerraInfinita = [{
+  nome: "Steve Rogers",
+  codinome: "Capitão América",
+  armaPrincipal: "Escudo Americano",
+  armaSecundaria: "",
+  velocidade: 85,
+  forca: 75,
+  resistencia: 80,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Tony Stark",
+  codinome: "Homem de Ferro",
+  armaPrincipal: "Armadura",
+  armaSecundaria: "",
+  velocidade: 90,
+  forca: 80,
+  resistencia: 85,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Bruce Baner",
+  codinome: "Hulk",
+  armaPrincipal: "",
+  armaSecundaria: "",
+  velocidade: 80,
+  forca: 95,
+  resistencia: 95,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Thanos",
+  codinome: "Thanos",
+  armaPrincipal: "",
+  armaSecundaria: "",
+  velocidade: 85,
+  forca: 98,
+  resistencia: 100,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Peter Parker",
+  codinome: "Homem Aranha",
+  armaPrincipal: "",
+  armaSecundaria: "",
+  velocidade: 90,
+  forca: 90,
+  resistencia: 90,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Thor",
+  codinome: "Thor",
+  armaPrincipal: "Mjolnir",
+  armaSecundaria: "StormBreaker",
+  velocidade: 90,
+  forca: 100,
+  resistencia: 98,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Stephen Strange",
+  codinome: "Doutor Estranho",
+  armaPrincipal: "olho de agamotto",
+  armaSecundaria: "",
+  velocidade: 80,
+  forca: 70,
+  resistencia: 75,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Natasha Romanova",
+  codinome: "Viuva Negra",
+  armaPrincipal: "olho de agamotto",
+  armaSecundaria: "",
+  velocidade: 85,
+  forca: 75,
+  resistencia: 77,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Wanda Maximoff",
+  codinome: "Feiticeira Escarlate",
+  armaPrincipal: "olho de agamotto",
+  armaSecundaria: "",
+  velocidade: 85,
+  forca: 75,
+  resistencia: 77,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+},
+{
+  nome: "Carol Danvers",
+  codinome: "Capita Marvel",
+  armaPrincipal: "",
+  armaSecundaria: "",
+  velocidade: 92,
+  forca: 87,
+  resistencia: 93,
+  descricao: function () {
+    return "Nome do personagem: " + this.nome + "\n"
+        + "Codinome do personagem: " + this.codinome + "\n"
+        + "Arma principal: " + this.armaPrincipal + "\n"
+        + "Arma secundaria: " + this.armaSecundaria + "\n"
+        + "Nivel de força: " + this.forca + "\n"
+        + "Nivel de velocidade: " + this.velocidade + "\n"
+        + "Nivel de resistencia: " + this.resistencia + "\n"
+  }
+}
+]
+
+
+function personagemMaisForte(personagens){
+  let personagemMaisForte = ''
+  let personagemMaisVeloz = ''
+  let personagemMaisResistente = ''
+  let maiorForca = 0;
+  let maiorVelocidade = 0;
+  let maiorResistencia = 0;
+  for (personagem in personagens) {
+    const atributoForca = personagens[personagem].forca
+    const atributoVelocidade = personagens[personagem].velocidade
+    const atributoResistencia = personagens[personagem].resistencia
+    if (atributoForca > maiorForca) {
+      maiorForca = atributoForca
+      personagemMaisForte = personagens[personagem].descricao()
+    }
+    if (atributoVelocidade > maiorVelocidade) {
+      maiorVelocidade = atributoVelocidade
+      personagemMaisVeloz = personagens[personagem].descricao()
+    }
+    if (atributoResistencia > maiorResistencia) {
+      maiorResistencia = atributoResistencia
+      personagemMaisResistente = personagens[personagem].descricao()
+    }
+  }
+  return personagemMaisForte, personagemMaisVeloz, personagemMaisResistente
+}
+
+
+function encontrarMaiorForca(personagens) {
+  let personagemMaisForte = ''
+  let maiorForca = 0;
+  for (personagem in personagens) {
+    const atributoForca = personagens[personagem].forca
+    if (atributoForca > maiorForca) {
+      maiorForca = atributoForca
+      personagemMaisForte = personagens[personagem].descricao()
+    }
+  }
+  return personagemMaisForte
+}
+
+
+function encontrarMaiorVelocidade(personagens) {
+  let personagemMaisVeloz = '';
+  let maiorVelocidade = 0;
+  for (personagem in personagens) {
+    const atributoVelocidade = personagens[personagem].velocidade
+    if (atributoVelocidade > maiorVelocidade) {
+      maiorVelocidade = atributoVelocidade
+      personagemMaisVeloz = personagens[personagem].descricao()
+    }
+  }
+  return personagemMaisVeloz
+}
+
+
+function encontrarMaiorResistencia(personagens) {
+  let personagemMaisResistente = ''
+  let maiorResistencia = 0;
+  for (personagem in personagens) {
+    const atributoResistencia = personagens[personagem].resistencia
+    if (atributoResistencia > maiorResistencia) {
+      maiorResistencia = atributoResistencia
+      personagemMaisResistente = personagens[personagem].descricao()
+    }
+  }
+  return personagemMaisResistente
+}
+
+
+const maiorForca = encontrarMaiorForca(guerraInfinita);
+const maiorVelocidade = encontrarMaiorVelocidade(guerraInfinita);
+const maiorResistencia = encontrarMaiorResistencia(guerraInfinita);
+
+console.log("Personagem com maior força: ");
+console.log(maiorForca);
+
+console.log("Personagem com maior velocidade: ");
+console.log(maiorVelocidade);
+
+console.log("Personagem com maior resistência: ");
+console.log(maiorResistencia);
